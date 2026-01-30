c2fea3e9fde136b1cbbf33a96fe58768d3362f79
twitch1026 <twitch10126@hotmail.com>
2026-01-30 09:21:01 +0000

    Add Procfile and install gunicorn for heroku deployment

--- brewbite/settings.py
+++ brewbite/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-je8$@bot9nw&kgi_2w*m!lig@x(rx6ptk7__d-*y752)w=i1@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
